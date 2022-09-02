44bad069a8d67c73481a41f897f1afdef4f8279d
Artezianec <artezianplanet@yandex.ru>
2022-09-02 09:04:33 +0300

    new ap

--- 22-js-advanced3/js/tryCatch.js
+++ 22-js-advanced3/js/tryCatch.js
@@ -1,39 +1,1 @@
-let person = {
-    name: 'Peter'
-};
-person = null;
-printName(person);
-
-console.log('=========');
-try {
-    let res = solution(0, 0);
-    console.log(res);
-} catch (e) {
-    console.log(e.name);
-    console.log(e.message);
-}
-
-function solution(a, b) {
-    if (a !== 0) {
-        return b / a;
-    } else {
-        if (b === 0) {
-            throw new Error('Message!!!');
-        }
-    }
-    throw new Error('No solution');
-}
-console.log(res);
-
-function printName(obj) {
-    try {
-        console.log(obj.name);
-        return;
-    } catch (error) {
-        console.log(error.name);
-        console.log(error.message);
-        console.log('Error');
-    } finally {
-        console.log('finally in printName');
-    }
-};
+let person = {    name: 'Peter'};person = null;printName(person);console.log('=========');try {    let res = solution(0, 0);    console.log(res);} catch (e) {    console.log(e.name);    console.log(e.message);}function solution(a, b) {    if (a !== 0) {        return b / a;    } else {        if (b === 0) {            throw new Error('Message!!!');        }    }    throw {message: 'no solution', name: 'object'}  ;}console.log(res);function printName(obj) {    try {        console.log(obj.name);        return;    } catch (error) {        console.log(error.name);        console.log(error.message);        console.log('Error');    } finally {        console.log('finally in printName');    }};
