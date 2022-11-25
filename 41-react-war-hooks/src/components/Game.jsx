daf88ead7f7e01792920b6bed858d73febacbc9f
Artezianec <artezianplanet@yandex.ru>
2022-11-25 09:02:36 +0200

    add 42-react-star-wars with hooks useState

--- 41-react-war-hooks/src/components/Game.jsx
+++ 41-react-war-hooks/src/components/Game.jsx
@@ -1,33 +1,34 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {result} from '../App'
 import {createDeck} from '../utils/constants'
 
-let compDeck;
-let playerDeck;
 const Game = ({name, changeGlobalScore, changePage}) => {
+	const compDeck = useRef([]);
+	const playerDeck = useRef([]);
 	const [compCard, setCompCard] = useState('Computer card');
 	const [playerCard, setPlayerCard] = useState('player card');
 	const [playerRes, setPlayerRes] = useState(0);
 	const [compRes, setCompRes] = useState(0);
 
 	useEffect(() => {
+
 		const deck = createDeck();
 		for (let i = deck.length - 1; i > 0; i--) {
 			let j = Math.floor(Math.random() * (i + 1));
 			[deck[i], deck[j]] = [deck[j], deck[i]];
 		}
-		compDeck = deck.slice(0, 26);
-		playerDeck = deck.slice(26, 52);
+		compDeck.current = deck.slice(0, 26);
+		playerDeck.current = deck.slice(26, 52);
 		return () => {
 			console.log('game unmounted');
 		}
-	},[])
+	}, [])
 
 
 	const handleClickNext = () => {
-		if (compDeck.length) {
-			const player = playerDeck.pop();
-			const comp = compDeck.pop();
+		if (compDeck.current.length) {
+			const player = playerDeck.current.pop();
+			const comp = compDeck.current.pop();
 			if (player.rank < comp.rank) {
 				setCompRes(prevCompRes => prevCompRes + 1);
 			}
--- 42-react-star-wars-hooks/src/App.js
+++ 42-react-star-wars-hooks/src/App.js
@@ -1,32 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import { navItems } from './utils/constants';
+import {navItems} from './utils/constants';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+	const [currentPage, setCurrentPage] = useState(navItems[0])
 
-    this.state = {
-      currentPage: navItems[0]
-    }
-  }
-
-  changePage = currentPage => {
-    this.setState({ currentPage })
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header changePage={this.changePage} />
-        <Main currentPage={this.state.currentPage} />
-        <Footer />
-      </div>
-    );
-  }
+	return (
+		<div className="container-fluid">
+			<Header changePage={setCurrentPage}/>
+			<Main currentPage={currentPage}/>
+			<Footer/>
+		</div>
+	);
 
 }
 
